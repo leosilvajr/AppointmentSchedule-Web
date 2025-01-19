dac935f43557fe0597f5ec15848e15ce0169baa1
Leonardo Silva <106756853+leosilvajr@users.noreply.github.com>
2025-01-19 07:44:11 -0300

    Removido arquivos iniciais e adicionado ICO na aba

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
+//Comando para renderizar dentro da div root o componente App
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <h1>Appointment Schedule</h1>
 )
