97cf566fb1ca9e27887185e18de3d97268393b39
Leonardo Silva <106756853+leosilvajr@users.noreply.github.com>
2025-01-19 22:43:05 -0300

    Finalziado Layout tela Inicial e adicionado Navbar

--- src/pages/appointments/appointments.jsx
+++ src/pages/appointments/appointments.jsx
@@ -3,7 +3,7 @@ import Navbar from "../../components/navbar/navbar"
 function Appointments() {
     return <>
         <Navbar/>
-        <h1>Appointments</h1>
+        <h1>Agendamentos</h1>
     </>
     
 }
